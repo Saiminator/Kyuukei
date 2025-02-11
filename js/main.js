24c4957bb3ebfe07ff2f3d71357b069326563ce0
Simon Minchk <42367509+Simon-Minchk@users.noreply.github.com>
2025-02-11 00:04:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Mobile Navigation Toggle for Mobile Nav Menu
   const navToggle = document.getElementById('nav-toggle');
   const navMobileMenu = document.getElementById('nav-mobile-menu');
-  
   if (navToggle && navMobileMenu) {
     navToggle.addEventListener('click', function() {
-      if (navMobileMenu.style.display === 'block') {
-        navMobileMenu.style.display = 'none';
-      } else {
-        navMobileMenu.style.display = 'block';
-      }
+      navMobileMenu.style.display = (navMobileMenu.style.display === 'block') ? 'none' : 'block';
     });
   }
 });
